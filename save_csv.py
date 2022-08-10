a762c8e6c7d9b81ed0324a80bb9a498e845c21b6
Deep Sarker <deepsarker01@gmail.com>
2022-08-10 23:21:38 +0600

    formatted file and used typhinting made moduler

--- save_csv.py
+++ save_csv.py
@@ -1,14 +1,20 @@
 import csv
 
-info_dict = {}
+
+def get_list_from_string(string: str) -> list:
+    each_info_list = []
+    for each_info in string.strip()[1:-1].split(","):
+        each_info_list.append(each_info.strip()[1:-1])
+    return each_info_list
+
+
 with open("data.txt", "r") as file:
-    csd_file = csv.writer(open("mytest.csv", "w"), )
+    info_dict = {}
+    csd_file = csv.writer(open("user_info.csv", "w"), )
     csd_file.writerow(["ID", "NAME", "PHONE", "EMAIL", "CITY", "STATE"])
     for each_info_in_string in file.readlines():
         if not each_info_in_string.isspace():
-            each_info_in_list = []
-            for each_info in each_info_in_string.strip()[1:-1].split(","):
-                each_info_in_list.append(each_info.strip()[1:-1])
+            each_info_in_list = get_list_from_string(each_info_in_string)
             if each_info_in_list[0] not in info_dict:
                 info_dict[each_info_in_list[0]] = each_info_in_list
 
